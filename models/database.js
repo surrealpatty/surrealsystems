0236622109f448f7463daff75b30dfd5ea350a96
Your Actual Name <your-email@example.com>
2025-09-03 13:41:23 -0700

    Enable SSL for Render Postgres

--- models/database.js
+++ models/database.js
@@ -4,13 +4,13 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
+  logging: false,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, // needed for Render Postgres
-    },
-  },
-  logging: false,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
