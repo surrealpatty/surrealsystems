51a91a8c2fc26a74c215ec70cd5eb266301cb343
Your Actual Name <your-email@example.com>
2025-09-09 16:40:10 -0700

    Enable SSL for Render Postgres

--- config/config.js
+++ config/config.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 module.exports = {
   development: {
     dialect: 'sqlite',
-    storage: './database.sqlite',
-    logging: false,
+    storage: './database.sqlite', // SQLite file for development
+    logging: false,               // Disable SQL logging
   },
   test: {
     dialect: 'sqlite',
-    storage: './database_test.sqlite',
+    storage: './database_test.sqlite', // SQLite file for testing
     logging: false,
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'DATABASE_URL',  // Render Postgres URL
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
