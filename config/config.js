5bbfad3a987b1a29d5d90676cf83981251479590
Your Actual Name <your-email@example.com>
2025-09-22 15:49:12 -0700

    Enable SSL for Render Postgres

--- config/config.js
+++ config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
   development: {
-    username: "root",
-    password: "your_mysql_password",
-    database: "codecrowds_dev",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false }
+    },
   },
   test: {
-    username: "root",
-    password: "your_mysql_password",
-    database: "codecrowds_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false }
+    },
   },
   production: {
-    username: "root",
-    password: "your_mysql_password",
-    database: "codecrowds_prod",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  }
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false }
+    },
+  },
 };
