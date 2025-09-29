e979d973860bba305f990a8d4e51ebd37207d1af
Your Actual Name <your-email@example.com>
2025-09-28 20:16:35 -0700

    Enable SSL for Render Postgres

--- models/index.js
+++ models/index.js
@@ -2,4 +2,7 @@ const User = require('./User');
 const Service = require('./Service');
 const Message = require('./Message');
 
+// You can define associations here if needed
+// e.g., User.hasMany(Service);
+
 module.exports = { User, Service, Message };
