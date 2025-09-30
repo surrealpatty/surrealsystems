e5098a6dcb6aef41d1c723a2d640df59ab429852
Your Actual Name <your-email@example.com>
2025-09-30 00:42:50 -0700

    Enable SSL for Render Postgres

--- src/routes/service.js
+++ src/routes/service.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const serviceController = require('../controllers/serviceController');
 const authenticateToken = require('../middlewares/authenticateToken');
 
-// Public: Get all services
+// ---------------- Public: Get all services ----------------
 router.get('/', serviceController.getAllServices);
 
-// Protected: Create, Update, Delete
+// ---------------- Protected: Create, Update, Delete ----------------
 router.post('/', authenticateToken, serviceController.createService);
 router.put('/:id', authenticateToken, serviceController.updateService);
 router.delete('/:id', authenticateToken, serviceController.deleteService);
