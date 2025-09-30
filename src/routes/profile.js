f688f3eb13c2952f8fc30361eb517e05829a68df
Your Actual Name <your-email@example.com>
2025-09-30 13:32:29 -0700

    Enable SSL for Render Postgres

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const { Op } = require('sequelize');
-const Message = require('../models/Message');
+const Message = require('../models/message');
 const User = require('../models/User');
 
 const router = express.Router();
